6f1269887191e4e8a0f8af5297b12ff075de25f7
gailin-p <glnpease@gmail.com>
2023-06-11 13:56:13 +0200

    Fix SQL * Remove NOT NULL from staging tables * Use SELECT DISTINCT to avoid duplicate rows * Remove double quotes from column names

--- sql_queries.py
+++ sql_queries.py
@@ -24,54 +24,54 @@ time_table_drop = "drop table if exists times"
 
 staging_events_table_create= ("""
 CREATE TABLE "staging_events" (
-    "artist" VARCHAR(200),
-    "auth" character varying(200) NOT NULL,
-    "firstName" character varying(200),
-    "gender" character varying(5),
-    "itemInSession" int NOT NULL,
-    "lastName" character varying(200),
-    "length" double precision,
-    "level" character varying(10) NOT NULL,
-    "location" character varying(200),
-    "method" character varying(5) NOT NULL,
-    "page" character varying(20) NOT NULL,
-    "registration" double precision, 
-    "sessionId" int NOT NULL,
-    "song" VARCHAR(200) DISTKEY,
-    "status" int NOT NULL,
-    "ts" bigint,
-    "userAgent" character varying(200),
-    "userId" int
+    artist VARCHAR(200),
+    auth character varying(200),
+    firstName character varying(200),
+    gender character varying(5),
+    itemInSession int,
+    lastName character varying(200),
+    length double precision,
+    level character varying(10),
+    location character varying(200),
+    method character varying(5),
+    page character varying(20),
+    registration double precision, 
+    sessionId int,
+    song VARCHAR(200) DISTKEY,
+    status int,
+    ts bigint,
+    userAgent character varying(200),
+    userId int
 );
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE "staging_songs" (
-    "num_songs" int, 
-    "artist_id" character varying(20) NOT NULL, 
-    "artist_latitude" double precision, 
-    "artist_longitude" double precision, 
-    "artist_location" character varying(200), 
-    "artist_name" VARCHAR(200) NOT NULL, 
-    "song_id" character varying(20) NOT NULL, 
-    "title" VARCHAR(200) NOT NULL DISTKEY, 
-    "duration" double precision NOT NULL, 
-    "year" int 
+    num_songs int, 
+    artist_id character varying(20), 
+    artist_latitude double precision, 
+    artist_longitude double precision, 
+    artist_location character varying(200), 
+    artist_name VARCHAR(200), 
+    song_id character varying(20), 
+    title VARCHAR(200) DISTKEY, 
+    duration double precision, 
+    year int 
 );
 """)
 
 # Q: what should the dist key be? 
 songplay_table_create = ("""
 CREATE TABLE "songplay" (
-    "songplay_id" int IDENTITY NOT NULL,
-    "start_time" timestamp NOT NULL REFERENCES times (start_time),
-    "user_id" int NOT NULL REFERENCES users (user_id), 
-    "level" character varying(10) NOT NULL,
-    "song_id" character varying(20) NOT NULL REFERENCES songs (song_id), 
-    "artist_id" character varying(20) NOT NULL, 
-    "session_id" int NOT NULL,
-    "location" character varying(200),
-    "user_agent" character varying(200)
+    songplay_id int IDENTITY NOT NULL,
+    start_time timestamp NOT NULL REFERENCES times (start_time),
+    user_id int NOT NULL REFERENCES users (user_id), 
+    level character varying(10) NOT NULL,
+    song_id character varying(20) NOT NULL REFERENCES songs (song_id), 
+    artist_id character varying(20) NOT NULL, 
+    session_id int NOT NULL,
+    location character varying(200),
+    user_agent character varying(200)
 );
 """)
 
@@ -79,11 +79,11 @@ CREATE TABLE "songplay" (
 # so we don't use the IDENTITY command to create it. 
 user_table_create = ("""
 CREATE TABLE "users" (
-    "user_id" int NOT NULL,
-    "first_name" character varying(200),
-    "last_name" character varying(200),
-    "gender" character varying(5),
-    "level" character varying(10) NOT NULL,
+    user_id int NOT NULL,
+    first_name character varying(200),
+    last_name character varying(200),
+    gender character varying(5),
+    level character varying(10) NOT NULL,
     PRIMARY KEY (user_id)
 )
 """)
@@ -92,35 +92,35 @@ CREATE TABLE "users" (
 # so we don't use the IDENTITY command to create it. 
 artist_table_create = ("""
 CREATE TABLE "artists" (
-    "artist_id" character varying(20) NOT NULL, 
-    "name" character varying(200) NOT NULL,
-    "location" character varying(200),
-    "latitude" double precision, 
-    "longitude" double precision,
+    artist_id character varying(20) NOT NULL, 
+    name character varying(200) NOT NULL,
+    location character varying(200),
+    latitude double precision, 
+    longitude double precision,
     PRIMARY KEY (artist_id)
 )
 """)
 
 song_table_create = ("""
 CREATE TABLE "songs" (
-    "song_id" character varying(20) NOT NULL, 
-    "title" character varying(200),
-    "artist_id" character varying(20) REFERENCES artists (artist_id),
-    "year" int,
-    "duration" double precision,
+    song_id character varying(20) NOT NULL, 
+    title character varying(200),
+    artist_id character varying(20) REFERENCES artists (artist_id),
+    year int,
+    duration double precision,
     PRIMARY KEY (song_id)
 )
 """)
 
 time_table_create = ("""
 CREATE TABLE "times" (
-    "start_time" timestamp NOT NULL,
-    "hour" int, 
-    "day" int, 
-    "week" int, 
-    "month" int, 
-    "year" int, 
-    "weekday" int,
+    start_time timestamp NOT NULL,
+    hour int, 
+    day int, 
+    week int, 
+    month int, 
+    year int, 
+    weekday int,
     PRIMARY KEY (start_time)
 )
 """)
@@ -143,21 +143,21 @@ staging_songs_copy = """
 
 songplay_table_insert = ("""
 INSERT INTO "songplay" (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-SELECT timestamp 'epoch' + ts * interval '1 second', userId, level, staging_songs.song_id, staging_songs.artist_id, sessionId, location, userAgent
+SELECT DISTINCT timestamp 'epoch' + ts * interval '1 second', userId, level, staging_songs.song_id, staging_songs.artist_id, sessionId, location, userAgent
 FROM staging_events JOIN staging_songs ON staging_events.artist = staging_songs.artist_name AND staging_events.song = staging_songs.title
 WHERE staging_events.page='NextSong';
 """)
 
 user_table_insert = ("""
 INSERT INTO "users" (user_id, first_name, last_name, gender, level)
-SELECT userId, firstName, lastName, gender, level
+SELECT DISTINCT userId, firstName, lastName, gender, level
 FROM staging_events
 WHERE staging_events.page='NextSong';
 """)
 
 song_table_insert = ("""
 INSERT INTO "songs" (song_id, title, artist_id, year, duration)
-SELECT song_id, title, artist_id, year, duration
+SELECT DISTINCT song_id, title, artist_id, year, duration
 FROM staging_songs
 WHERE title IN
 (
@@ -167,13 +167,13 @@ SELECT title FROM staging_events
 
 artist_table_insert = ("""
 INSERT INTO "artists" (artist_id, name, location, latitude, longitude)
-SELECT staging_songs.artist_id, staging_songs.artist_name, staging_songs.artist_location, staging_songs.artist_latitude, staging_songs.artist_longitude
+SELECT DISTINCT staging_songs.artist_id, staging_songs.artist_name, staging_songs.artist_location, staging_songs.artist_latitude, staging_songs.artist_longitude
 FROM songplay JOIN staging_songs ON songplay.artist_id = staging_songs.artist_id
 """)
 
 time_table_insert = ("""
 INSERT INTO "times" (start_time, hour, day, week, month, year, weekday)
-SELECT start_time, DATEPART(HOUR, start_time), DATEPART(DAY, start_time), DATEPART(WEEK, start_time), DATEPART(MONTH, start_time), DATEPART(YEAR, start_time), DATEPART(WEEKDAY, start_time)
+SELECT DISTINCT start_time, DATEPART(HOUR, start_time), DATEPART(DAY, start_time), DATEPART(WEEK, start_time), DATEPART(MONTH, start_time), DATEPART(YEAR, start_time), DATEPART(WEEKDAY, start_time)
 FROM songplay 
 """)
                      
